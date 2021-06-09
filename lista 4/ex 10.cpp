d6d56b85553f7afeebb7bf3736ae600930ba46b3
christian <80042310+crstyhs@users.noreply.github.com>
2021-06-09 15:42:58 -0300

    Update ex 10.cpp

--- lista 4/ex 10.cpp
+++ lista 4/ex 10.cpp
@@ -7,21 +7,21 @@ int main(){
 	float porcentagem[3];
 
 do{
-	printf("informe o tipo de vinho a ser adicionado ao estoque,sendo: ‘T’ para tinto, ‘B’ para branco e ‘R’ para rosê: ");
+	printf("informe o tipo de vinho a ser adicionado ao estoque,sendo: â€˜Tâ€™ para tinto, â€˜Bâ€™ para branco e â€˜Râ€™ para rosÃª: ");
 	setbuf(stdin,0);
 	scanf("%c",&opcao);
 	switch(opcao) {
-	case 'T': printf("qual é a quantidade a ser adicionada: ");
+	case 'T': printf("qual Ã© a quantidade a ser adicionada: ");
 	scanf("%i",&quantidade);
 	quantidade_total=quantidade_total+quantidade;
 	quantidade_tinto=quantidade_tinto+quantidade;break;
 	
-	case 'B': printf("qual é a quantidade a ser adicionada: ");
+	case 'B': printf("qual Ã© a quantidade a ser adicionada: ");
 	scanf("%i",&quantidade);
 	quantidade_total=quantidade_total+quantidade;
 	quantidade_branco=quantidade_branco+quantidade;break;
 	
-	case 'R': printf("qual é a quantidade a ser adicionada: ");
+	case 'R': printf("qual Ã© a quantidade a ser adicionada: ");
 	scanf("%i",&quantidade);
 	quantidade_total=quantidade_total+quantidade;
 	quantidade_rose=quantidade_rose+quantidade;break;}}
@@ -29,7 +29,7 @@ do{
 	porcentagem[0]=(quantidade_tinto/quantidade_total)*100;
 	porcentagem[1]=(quantidade_branco/quantidade_total)*100;
 	porcentagem[2]=(quantidade_rose/quantidade_total)*100;
-	printf("A porcentagem de vinho tinto é %fl\n",porcentagem[0]);
-	printf("A porcentagem de vinho branco é %fl\n",porcentagem[1]);
-	printf("A porcentagem de vinho rose é %fl\n",porcentagem[2]);
+	printf("A porcentagem de vinho tinto Ã© %f\n",porcentagem[0]);
+	printf("A porcentagem de vinho branco Ã© %f\n",porcentagem[1]);
+	printf("A porcentagem de vinho rose Ã© %f\n",porcentagem[2]);
 }
