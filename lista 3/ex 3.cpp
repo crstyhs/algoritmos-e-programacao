fa0b36f23ecbfd330b8d838730b311ca864d3c69
christian <80042310+crstyhs@users.noreply.github.com>
2021-06-09 15:52:37 -0300

    Update ex 3.cpp

--- lista 3/ex 3.cpp
+++ lista 3/ex 3.cpp
@@ -5,16 +5,16 @@ int main(){
 	float altura[10];
 	int valor=0,vetor1=1,vetor2=0;
 	printf("Digete as alturas: ");
-	scanf("%fl",&altura[0]);
-	scanf("%fl",&altura[1]);
-	scanf("%fl",&altura[2]);
-	scanf("%fl",&altura[3]);
-	scanf("%fl",&altura[4]);
-	scanf("%fl",&altura[5]);
-	scanf("%fl",&altura[6]);
-	scanf("%fl",&altura[7]);
-	scanf("%fl",&altura[8]);
-	scanf("%fl",&altura[9]);
+	scanf("%f",&altura[0]);
+	scanf("%f",&altura[1]);
+	scanf("%f",&altura[2]);
+	scanf("%f",&altura[3]);
+	scanf("%f",&altura[4]);
+	scanf("%f",&altura[5]);
+	scanf("%f",&altura[6]);
+	scanf("%f",&altura[7]);
+	scanf("%f",&altura[8]);
+	scanf("%f",&altura[9]);
 	while(vetor1!=10){
 	vetor1=1;
 	valor=0;
@@ -22,5 +22,5 @@ int main(){
 	valor=altura[vetor1]-altura[vetor2];
 	vetor1++;}vetor2++;}
 	vetor2--;
-	printf("A menor altura é %fl",altura[vetor2]);
+	printf("A menor altura Ã© %f",altura[vetor2]);
 	return 0;}
